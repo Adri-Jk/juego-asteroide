6d74369815caedb2685514c6f365b5c356fa289a
Adri-Jk <adrianarodriguezmiranda01@gmail.com>
2026-04-08 13:30:50 -0400

    Mi index.html

---
+++ juego.js
@@ -0,0 +1,31 @@
+const canvas = document.querySelector('canvas');
+const c = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+c.fillStyle = 'black';
+c.fillRect(0,0, canvas.width, canvas.height);
+const nave = {
+    x: canvas.width/2,
+    y: canvas.height/2,
+    vx:0, vy:0,
+    size: 20,
+    alive:true,
+    invencivilidad:0,
+}
+function dibujarNave(x,y,angulo,thrusting){
+  c.save();
+  c.translate(x,y);
+  c.rotate(angulo);
+  c.beginPath();
+
+  c.moveTo(20,0);
+  c.lineTo(-10,-10);
+  c.lineTo(-10,10);
+  c.closePath();
+  c.strokeStyle='white';
+  c.stroke();
+  c.restore();
+}
+dibujarNave(nave.x, nave.y, 0,false);
+ console.log(c);
+console.log(canvas);
